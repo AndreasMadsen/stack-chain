a5e90d8a23a56c2045bf3ade5f639a3018ec168f
Andreas Madsen <amwebdk@gmail.com>
2015-10-29 22:56:08 +0100

    [test] resolve from dirname

--- test/simple/uglify.js
+++ test/simple/uglify.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 test("can be uglified", function (t) {
   var files = ['format.js', 'index.js', 'stack-chain.js'].map(function (filename) {
-    return path.resolve('../../' + filename);
+    return path.resolve(__dirname, '../../' + filename);
   });
   uglify.minify(files);
   t.end()
